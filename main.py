708d3e880cceccbd0287ce3a50fd9d835851e005
Yan Zhan <yanzhan91@gmail.com>
2018-06-06 18:39:27 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import uuid
+
+from flask import Flask, jsonify
+from datetime import datetime
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def get_word():
+    return jsonify({
+        "uid": uuid.uuid4(),
+        "updateDate": datetime.today().strftime('%Y-%m-%dT00:00:00.0Z'),
+        "titleText": "ChineseWordADay",
+        "mainText": "",
+        "streamUrl": "https://s3.amazonaws.com/chinese-word-a-day/current/sample.mp3",
+        "redirectionUrl": "https://s3.amazonaws.com/chinese-word-a-day/redirect.html"
+    })
+
+
+if __name__ == '__main__':
+    app.run()
